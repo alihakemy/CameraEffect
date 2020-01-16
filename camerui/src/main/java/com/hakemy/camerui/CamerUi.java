ee7b9ef2acd35fc249026d4ee64902baa9bae481
ali <alisamihakemy@gmail.com>
2020-01-16 21:44:05 +0200

    Initial commit

---
+++ camerui/src/main/java/com/hakemy/camerui/CamerUi.java
@@ -0,0 +1,13 @@
+package com.hakemy.camerui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CamerUi extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camer_ui);
+    }
+}
